03445b2ccb2bacf6677f217e152e17131d655d7d
Slava Bereza <mail@betula.co>
2022-07-29 17:17:45 +0700

    fix: update binding typings

--- react/src/index.d.ts
+++ react/src/index.d.ts
@@ -1,12 +1,12 @@
-import { Box } from 'remini';
+import { BoxReadable } from 'remini';
 
 export declare const observe: any;
-export declare const useBox: <P>(box: Box<P>) => P;
+export declare const useBox: <P>(box: BoxReadable<P>) => P;
 export declare const useBoxes: {
-  <A>(boxes: [Box<A>]): [A];
-  <A,B>(boxes: [Box<A>,Box<B>]): [A,B];
-  <A,B,C>(boxes: [Box<A>,Box<B>,Box<C>]): [A,B,C];
-  <A,B,C,D>(boxes: [Box<A>,Box<B>,Box<C>,Box<D>]): [A,B,C,D];
-  <A,B,C,D,E>(boxes: [Box<A>,Box<B>,Box<C>,Box<D>,Box<E>]): [A,B,C,D,E];
-  <A,B,C,D,E,F>(boxes: [Box<A>,Box<B>,Box<C>,Box<D>,Box<E>,Box<F>]): [A,B,C,D,E,F];
+  <A>(boxes: [BoxReadable<A>]): [A];
+  <A,B>(boxes: [BoxReadable<A>,BoxReadable<B>]): [A,B];
+  <A,B,C>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>]): [A,B,C];
+  <A,B,C,D>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>,BoxReadable<D>]): [A,B,C,D];
+  <A,B,C,D,E>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>,BoxReadable<D>,BoxReadable<E>]): [A,B,C,D,E];
+  <A,B,C,D,E,F>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>,BoxReadable<D>,BoxReadable<E>,BoxReadable<F>]): [A,B,C,D,E,F];
 }
