2715c8eb4910ad6f469d014d7d6a531655c0898f
Slava Bereza <mail@betula.co>
2022-07-30 15:40:00 +0700

    feat: typings updated

--- preact/src/index.d.ts
+++ preact/src/index.d.ts
@@ -1,12 +1,18 @@
-import { BoxReadable } from 'remini';
+import { Box } from 'remini';
 
-export declare const observe: any;
-export declare const useBox: <P>(box: BoxReadable<P>) => P;
+type Observe = {
+  <R>(component: () => R): () => R;
+  <Props, R>(component: (props: Props) => R): (props: Props) => R;
+  <Props, Ref, R>(component: (props: Props, ref: Ref) => R): (props: Props, ref: Ref) => R;
+}
+
+export declare const observe: Observe;
+export declare const useBox: <P>(box: Box<P, 'readable'>) => P;
 export declare const useBoxes: {
-  <A>(boxes: [BoxReadable<A>]): [A];
-  <A,B>(boxes: [BoxReadable<A>,BoxReadable<B>]): [A,B];
-  <A,B,C>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>]): [A,B,C];
-  <A,B,C,D>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>,BoxReadable<D>]): [A,B,C,D];
-  <A,B,C,D,E>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>,BoxReadable<D>,BoxReadable<E>]): [A,B,C,D,E];
-  <A,B,C,D,E,F>(boxes: [BoxReadable<A>,BoxReadable<B>,BoxReadable<C>,BoxReadable<D>,BoxReadable<E>,BoxReadable<F>]): [A,B,C,D,E,F];
+  <A>(boxes: [Box<A, 'readable'>]): [A];
+  <A,B>(boxes: [Box<A, 'readable'>,Box<B, 'readable'>]): [A,B];
+  <A,B,C>(boxes: [Box<A, 'readable'>,Box<B, 'readable'>,Box<C, 'readable'>]): [A,B,C];
+  <A,B,C,D>(boxes: [Box<A, 'readable'>,Box<B, 'readable'>,Box<C, 'readable'>,Box<D, 'readable'>]): [A,B,C,D];
+  <A,B,C,D,E>(boxes: [Box<A, 'readable'>,Box<B, 'readable'>,Box<C, 'readable'>,Box<D, 'readable'>,Box<E, 'readable'>]): [A,B,C,D,E];
+  <A,B,C,D,E,F>(boxes: [Box<A, 'readable'>,Box<B, 'readable'>,Box<C, 'readable'>,Box<D, 'readable'>,Box<E, 'readable'>,Box<F, 'readable'>]): [A,B,C,D,E,F];
 }
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,35 +1,33 @@
 
 type Mode = 'writable' | 'readable';
 
-export type BoxReadable<T, M extends Mode = 'readable'> = [
+export type Box<T, M extends Mode = 'writable'> = [
   () => T,
   M extends 'writable'
     ? ((value: T) => void)
     : ((value: T) => void) | void
 ]
 
-export type Box<T> = BoxReadable<T, 'writable'>;
-
 export declare const box: <T>(value: T) => Box<T>;
 export declare const update: <P>(box: Box<P>, fn: (value: P) => P) => void;
-export declare const read: <P>(box: BoxReadable<P>) => P;
+export declare const read: <P>(box: Box<P, 'readable'>) => P;
 export declare const write: <P>(box: Box<P>, value: P) => void;
 
 export declare const wrap: {
   <P>(
-    getter: (() => P) | BoxReadable<P>,
-  ): BoxReadable<P>;
+    getter: (() => P) | Box<P, 'readable'>,
+  ): Box<P, 'readable'>;
   <P>(
-    getter: (() => P) | BoxReadable<P>,
+    getter: (() => P) | Box<P, 'readable'>,
     setter: ((value: P) => void) | Box<P>
   ): Box<P>;
 }
 
-export declare const readonly: <P>(box: Box<P>) => BoxReadable<P>;
+export declare const readonly: <P>(box: Box<P>) => Box<P, 'readable'>;
 
 type Subscriber = {
   <P>(
-    target: (() => P) | BoxReadable<P>,
+    target: (() => P) | Box<P, 'readable'>,
     listener: (value: P, prev: P | void) => void
   ): () => void;
 }
@@ -39,13 +37,13 @@ export declare const once: Subscriber;
 export declare const sync: Subscriber;
 
 export declare const map: {
-  <T, R>(box: BoxReadable<T>, fn: (value: T) => R): BoxReadable<R>;
+  <P, R>(box: Box<P, 'readable'>, fn: (value: P) => R): Box<R, 'readable'>;
 }
 
-type Zone = {
+type Area = {
   <T>(fn: () => T): T;
   fn: <M extends ((...args: any[]) => any)>(fn: M) => M;
 }
 
-export declare const batch: Zone;
-export declare const untrack: Zone;
+export declare const batch: Area;
+export declare const untrack: Area;
